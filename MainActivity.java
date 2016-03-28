08540988840d158f8f03983a93f98ce8b41e88fb
simsarulhaqv <simsarulhaqv@gmail.com>
2016-03-28 13:39:50 +0530

    Add Main activity

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lostnfound.lostnfound;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by simsar on 28/3/16.
+ */
+
+public class  MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
