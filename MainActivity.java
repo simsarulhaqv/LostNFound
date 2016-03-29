78c475530d5097a350cd0e451b5dcab403cdc224
simsarulhaqv <simsarulhaqv@gmail.com>
2016-03-29 15:59:01 +0530

    Add MainActivity

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lostnfound.lostnfound;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by simsar on 28/3/16.
@@ -12,5 +16,17 @@ public class  MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+                                       @Override
+                                       public void onClick(View v) {
+                                           Intent post = new Intent(MainActivity.this,postFind.class);
+                                           startActivity(post);
+                                       }
+                                   }
+
+        );
     }
 }
